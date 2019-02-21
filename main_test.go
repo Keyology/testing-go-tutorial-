ee2df4ec11eb05288716b6172c0f25bff95a8353
Keoni Murray <keonimurray45@gmail.com>
2019-02-21 14:46:42 -0800

    written table test

--- main_test.go
+++ main_test.go
@@ -4,9 +4,35 @@ import (
 	"testing"
 )
 
+// bottlenecks are performance blockers 
+// problems that make your application slower
+// 
+
 func TestCalculate(t *testing.T){
-	if Calculate(2) != 4{
-		t.Error("Expected error 2 + 2 to equal 4")
+
+	var tests = []struct {
+		input int
+		expected int
+	}{
+		{2,4}, 
+		{-1, 1},
+		{0, 2}, 
+		{-5, -3}, 
+		{99999, 100001}, 
 	}
 
+	for _, test := range tests{
+		if output := Calculate(test.input);  output != test.expected{
+			t.Error("Test Failed: {} inputted, {} expected, recieved: {}", test.input, test.expected, output)
+		}
+	}
+
+
+
 }
+
+// resources 
+
+// writing test in go tutorial 
+
+//https://tutorialedge.net/golang/intro-testing-in-go/
